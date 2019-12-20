42da1ff219b78c1884c675725de444f3e2dbee73
KaneKopetski <KaneKopetski@users.noreply.github.com>
2019-12-20 13:25:29 -0500

    Remove paging from main dev branch

--- src/main/java/com/zipcode/group3blog/controller/CommentController.java
+++ src/main/java/com/zipcode/group3blog/controller/CommentController.java
@@ -1,12 +1,8 @@
 package com.zipcode.group3blog.controller;
 
 import com.zipcode.group3blog.dto.CommentDTO;
-import com.zipcode.group3blog.model.Comment;
-import com.zipcode.group3blog.repository.CommentRepository;
 import com.zipcode.group3blog.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,8 +16,6 @@ public class CommentController {
 
     @Autowired
     private CommentService commentService;
-    @Autowired
-    private CommentRepository commentRepository;
 
     @PostMapping
     public ResponseEntity createComment(@RequestBody CommentDTO commentDTO){
@@ -31,8 +25,8 @@ public class CommentController {
 
     @Valid
     @GetMapping
-    public ResponseEntity<Page<Comment>> showAllComments(Pageable pageable) {
-        Page<Comment> allComments = commentRepository.findAllBy(pageable);
+    public ResponseEntity<List<CommentDTO>> showAllComments() {
+        List<CommentDTO> allComments = commentService.showAllComments();
         return new ResponseEntity<>(allComments, HttpStatus.OK);
     }
 
