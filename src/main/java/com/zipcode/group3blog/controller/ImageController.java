c9823dc22cea06a199df63584a25d9f7540be877
KaneKopetski <KaneKopetski@users.noreply.github.com>
2019-12-24 18:28:12 -0500

    Add images

--- src/main/java/com/zipcode/group3blog/controller/ImageController.java
+++ src/main/java/com/zipcode/group3blog/controller/ImageController.java
@@ -1,4 +1,38 @@
 package com.zipcode.group3blog.controller;
 
+import com.zipcode.group3blog.model.Image;
+import com.zipcode.group3blog.repository.ImageRepository;
+import com.zipcode.group3blog.service.ImageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping(path = "/api/images")
 public class ImageController {
+
+    @Autowired
+//    ImageService imageService;
+    @Autowired
+    ImageRepository imageRepository;
+
+    @PostMapping("/upload")
+    public Image uploadImage(@RequestParam("myFile") MultipartFile file) throws IOException {
+        Image img = new Image( file.getOriginalFilename(), file.getContentType(), file.getBytes());
+        final Image savedImage = imageRepository.save(img);
+        return savedImage;
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Image> getSingleImage(@PathVariable @RequestBody Long id) {
+        return new ResponseEntity(imageRepository.findById(id), HttpStatus.OK);
+    }
+
+
+
 }
---
+++ src/main/java/com/zipcode/group3blog/service/ImageService.java
@@ -0,0 +1,4 @@
+package com.zipcode.group3blog.service;
+
+public class ImageService {
+}
